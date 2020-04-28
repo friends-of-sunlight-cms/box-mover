c0517de52df6f9fd0cd673d0546715506c10f900
Jirka Daněk <jirka.danek@jdanek.eu>
2020-04-28 16:54:30 +0200

    Typo

--- plugins/extend/boxmover/Resources/languages/cs.php
+++ plugins/extend/boxmover/Resources/languages/cs.php
@@ -2,7 +2,7 @@
 
 return array(
     'caption' => 'Hromadné přesouvání boxů do aktivního motivu',
-    'move.to.theme' => 'Přenout do aktivního motivu',
+    'move.to.theme' => 'Přesunout do aktivního motivu',
     'convert.slots' => 'Konvertovat neexistující sloty',
     'row.title' => 'Titulek',
     'submit' => 'Přesunout',
