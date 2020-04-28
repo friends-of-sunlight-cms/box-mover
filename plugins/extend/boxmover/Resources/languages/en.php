0851d0eec6822ce18cbec862764b81b62485b510
Jirka Daněk <jirka.danek@jdanek.eu>
2020-04-28 16:54:46 +0200

    Add EN translation

---
+++ plugins/extend/boxmover/Resources/languages/en.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'caption' => 'Bulk moving boxes to the active theme',
+    'move.to.theme' => 'Move to active theme',
+    'convert.slots' => 'Convert non-existent slots',
+    'row.title' => 'Title',
+    'submit' => 'Move',
+);
